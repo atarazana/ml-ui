dad8bc52aefeeef27ac75f1975566a81597edd86
cvicens <carlos.vicens.alonso@gmail.com>
2023-11-04 12:16:30 +0100

    uuid package removed

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const { v4: uuidv4 } = require('uuid');
-
 const url = process.env.BACKEND_URL || 'http://127.0.0.1:5000'; // Replace with your API URL
 
 // Middleware to parse JSON data
